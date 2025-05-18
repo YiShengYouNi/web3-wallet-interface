e60467e5e903227070726795480af4dfbc156f06
weiheng <heng.wei@definesys.com>
2025-05-18 23:05:18 +0800

    feat: 封装 useWalletConnect

---
+++ src/hooks/useWalletConnect.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { useEffect } from 'react'
+import { useAccount, useWalletClient } from 'wagmi'
+import { walletStore } from '@/stores/walletStore';
+
+/**
+ * useWalletConnect
+ * 
+ * 自定义 Hook，桥接 wagmi 钱包状态与 Zustand 全局状态
+ * 支持链 ID / 账户监听
+ */
+export const useWalletConnect = () => {
+
+  const { address, chainId, isConnected } = useAccount();
+  const { data: walletClient } = useWalletClient();
+
+
+  const setWalletInfo = walletStore(s => s.setWalletInfo);
+  //   const setWalletInfo = useCallback(
+  //   (address: string | null, chainId: string | null) => {
+  //     walletStore.getState().setWalletInfo(address, chainId);
+  //   },
+  //   []
+  // );
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    // 监听 wagmi 连接状态变化，并同步到 Zustand 状态中
+    setWalletInfo(address ?? null, chainId?.toString() ?? null);
+
+  }, [address, chainId, isConnected, walletClient]);
+
+  return walletStore();
+};
